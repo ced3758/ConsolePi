d0505269502b78fd2d86a1a4cdd19d66f310885a
Cedric Charles <cedric.charlesrous@gmail.com>
2019-04-11 14:21:45 +0200

    SSHDeploy Ok

--- ConsolePi/Program.cs
+++ ConsolePi/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Threading;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Abstractions;
+using Unosquare.WiringPi;
 
 namespace ConsolePi
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Thread.Sleep(2000);
+            Console.WriteLine("Close is possible");
+
+            Pi.Init<BootstrapWiringPi>();
+
+            var pin = Pi.Gpio[P1.Pin19];
+            pin.PinMode = GpioPinDriveMode.Input;
+            pin.RegisterInterruptCallback(EdgeDetection.RisingEdge, Callback);
+
+            Console.ReadKey();
+        }
+
+        private static void Callback()
+        {
+            Console.WriteLine("toto");
         }
     }
 }
